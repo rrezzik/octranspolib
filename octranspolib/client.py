9301c8ab8e3e5acf65ae7f739992ef47682387f6
Rafik Rezzik <rafik.rezzik@gmail.com>
2015-02-23 23:28:02 -0500

    Finished the interfaces for the three main methods exposed by the OC Transept api.

--- octranspolib/client.py
+++ octranspolib/client.py
@@ -3,82 +3,84 @@ Provides the main interfaces for the OC Transpo API
 """
 
 import logging
-import functools
 from octranspolib.protocol import Api
+from octranspolib import model
+from octranspolib import parser
 
 class Client(object):
-	"""
-	Main client class. It will interact with the OC Transpo API and expose interfaces 
-	"""
+    """
+    Main client class. It will interact with the OC Transpo API and expose interfaces
+    """
 
-	def __init__(self, app_id, api_key):
-		"""
-		Contructor will initialize a new client object.
+    def __init__(self, app_id, api_key):
+        """
+        Contructor will initialize a new client object.
 
-		:param app_id: This is the application ID, you should send with each API request.
-		:type app_id: str
+        :param app_id: This is the application ID, you should send with each API request.
+        :type app_id: str
 
-		:param api_key: These are application keys used to authenticate requests.
-		:type api_key: str
+        :param api_key: These are application keys used to authenticate requests.
+        :type api_key: str
 
-		"""
+        """
 
-		# Initialize the logger
-		self.log = logging.getLogger('{0.__module__}.{0.__name__})'.format(self.__class__))
+        # Initialize the logger
+        self.log = logging.getLogger('{0.__module__}.{0.__name__})'.format(self.__class__))
 
-		self.protocol = Api(app_id=app_id, api_key=api_key)
+        self.protocol = Api(app_id=app_id, api_key=api_key)
 
-	def get_route_summary_for_stop(self, stop_number):
-		"""
-		Retrieves the routes for a given stop number
-		
-		https://api.octranspo1.com/v1.2/GetRouteSummaryForStop
+    def get_route_summary_for_stop(self, stop_number):
+        """
+        Retrieves the routes for a given stop number
 
-		:param stop_number: Required. 4-digit stop number found on bus stops.
-		:type stop_number int
+        https://api.octranspo1.com/v1.2/GetRouteSummaryForStop
 
-		:return Iterator of :class:`octranspolib.model.Route` objects
-		:rtype: :calss:`BatchedResultsIterator`
-		"""
+        :param stop_number: Required. 4-digit stop number found on bus stops.
+        :type stop_number int
 
-		result_fetcher = functools.partial(self.protocol.post,
-                                           '/GetRouteSummaryForStop',
-                                           stopNo=stop_number)
-		result_fetcher()
+        :return :class:`octranspolib.model.StopSummary` object
+        :rtype: :class:`StopSummary`
+        """
 
+        raw = self.protocol.post('/GetRouteSummaryForStop', stopNo=stop_number)
+        json_obj = parser.StopSummary(raw.content).parse()
 
-	def get_next_trips_for_stop(self, stop_number, route_number):
-		"""
-		Retrieves next three trips on the route for a given stop number
-
-		https://api.octranspo1.com/v1.2/GetNextTripsForStop
-
-		:param route_number: Required. Bus route number.
-		:param stop_number: Requited. 4-digit stop number found on bus stops.
-
-		"""
-
-		result_fetcher = functools.partial(self.protocol.post,
-											'/GetNextTripsForStop',
-											stopNo=stop_number,
-											routeNo=route_number)
-
-		result_fetcher()
+        return model.StopSummary.deserialize(json_obj)
 
 
-	def get_next_trips_for_stop_all_routes(self, stop_number, format=None):
-		"""
-		Retrieves next three trips on the route for a given stop number
+    def get_next_trips_for_stop(self, stop_number, route_number):
+        """
+        Retrieves next three trips on the route for a given stop number
 
-		https://api.octranspo1.com/v1.2/GetNextTripsForStop
+        https://api.octranspo1.com/v1.2/GetNextTripsForStop
 
-		:param route_number: Required. Bus route number.
-		:param stop_number: Requited. 4-digit stop number found on bus stops.
+        :param route_number: Required. Bus route number.
+        :param stop_number: Requited. 4-digit stop number found on bus stops.
 
-		"""
+        :return :class:`octranspolib.model.NextTrips` object
+        :rtype: :class:`NextTrips`
+        """
 
-		result_fetcher = functools.partial(self.protocol.post,
-											'/GetNextTripsForStopAllRoutes',
-											stopNo=stop_number)
+        raw = self.protocol.post('/GetNextTripsForStop', stopNo=stop_number, routeNo=route_number)
+        json_obj = parser.NextTrips(raw.content).parse()
 
-		result_fetcher()
+        return model.NextTrips.deserialize(json_obj)
+
+
+    def get_next_trips_for_stop_all_routes(self, stop_number, format=None):
+        """
+        Retrieves next three trips for all routes for a given stop number.
+
+        https://api.octranspo1.com/v1.2/GetNextTripsForStop
+
+        :param route_number: Required. Bus route number.
+        :param stop_number: Requited. 4-digit stop number found on bus stops.
+
+        :return :class:`octranspolib.model.NextTrips` object
+        :rtype: :class:`NextTrips`
+        """
+
+        raw = self.protocol.post('/GetNextTripsForStopAllRoutes', stopNo=stop_number)
+        json_obj = parser.NextTrips(raw.content).parse()
+
+        return model.NextTrips.deserialize(json_obj)
